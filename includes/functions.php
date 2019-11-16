85471bb72c885f215d523f3f61d3e43381b2bdd8
Thiago Martins <streetplayers.martins@gmail.com>
2019-11-15 22:38:59 -0300

    Segunda alteracao

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,7 @@
 <?php
 //mostar data em portugues
+//nova alteracao do git
+
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 date_default_timezone_set('America/Sao_Paulo');
 
